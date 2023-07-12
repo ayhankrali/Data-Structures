860164128b2fd2863b3dbc3cabdf709555246c95
Ayhan Krala <codewolf27@gmail.com>
2023-07-12 14:00:03 +0300

    add files

---
+++ src/arraylist/ArrayListExample.java
@@ -0,0 +1,21 @@
+package arraylist;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class ArrayListExample {
+    public static void main(String[] args) {
+        ArrayList<Object> list = new ArrayList<>();
+        list.add("Hello");
+        list.add(5);
+        list.add(2727.66);
+        list.add(2727.66);
+        list.add(new Date());
+
+        for (int i = 0; i <list.size() ; i++) {
+            Object value = new Object();
+            System.out.printf("Index=%d; Value=%s\n", i, value);
+        }
+
+    }
+}

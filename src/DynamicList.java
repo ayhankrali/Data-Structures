c058fa413ea99e522cc73cc573092f586554da4f
Ayhan Krala <codewolf27@gmail.com>
2023-07-12 13:17:40 +0300

    add files

---
+++ src/DynamicList.java
@@ -0,0 +1,25 @@
+public class DynamicList {
+
+
+    private class Node{
+        Object element ;
+        Node next ;
+
+        public Node(Object element,Node next) {
+            this.element = element;
+            this.next = next ;
+        }
+    }
+
+    private Node head;
+
+    private Node tail;
+
+    private int count;
+
+    public DynamicList(){
+        this.count = 0 ;
+        this.tail = null ;
+        this.head = null ;
+    }
+}

00aa78c196f3b9e84ec97ee3b62d17151e4a7194
Ayhan Krala <codewolf27@gmail.com>
2023-07-12 13:41:38 +0300

    add remove methods

--- src/DynamicList.java
+++ src/DynamicList.java
@@ -1,13 +1,12 @@
 public class DynamicList {
 
 
-    private class Node{
+    private static class Node{
         Object element ;
         Node next ;
 
-        public Node(Object element,Node next) {
+        public Node(Object element, Node tail) {
             this.element = element;
-            this.next = next ;
         }
     }
 
@@ -24,6 +23,125 @@ public class DynamicList {
     }
 
     public void add(Object item){
+        if (head == null){
+            head = new Node(item, tail);
+            tail = head ;
+        }else {
+            Node newNode = new Node(item, tail);
+            tail = newNode ;
+        }
+        count ++ ;
+    }
+
+
+    public Object remove (int index){
+        if (index >= count || index < 0 ){
+            throw new IndexOutOfBoundsException("Invalid index " + index);
+        }
+
+        int currentIndex = 0 ;
+        Node currentNode = head ;
+        Node prevNode = null ;
+
+        while (currentIndex < index) {
+
+            prevNode = currentNode;
+
+            currentNode = currentNode.next;
+
+            currentIndex++;
+
+        }
+
+        //remove the element
+        if (count == 0) {
+
+            head = null;
+
+            tail = null;
+
+        } else if (prevNode == null) {
+
+            head = currentNode.next;
+
+        } else {
+
+            prevNode.next = currentNode.next;
+
+        }
+
+        return currentNode.element;
+    }
+
+    public int remove(Object item){
+
+        // Find the element containing searched item
+
+        int currentIndex = 0;
+
+        Node currentNode = head;
+
+        Node prevNode = null;
+
+        while (currentNode != null) {
+
+            if ((currentNode.element!=null &&
+
+                    currentNode.element.equals(item)) ||
+
+                    (currentNode.element==null) && (item==null)){
+
+                break;
+
+            }
+
+            prevNode = currentNode;
+
+            currentNode = currentNode.next;
+
+            currentIndex++;
+
+        }
+
+
+
+        if (currentNode != null) {
+
+            // Element is found in the list. Remove it
+
+            count--;
+
+            if (count==0) {
+
+                head = null;
+
+                tail = null;
+
+            } else if (prevNode == null) {
+
+                head = currentNode.next;
+
+            } else {
+
+                prevNode.next = currentNode.next;
+
+            }
+
+            return currentIndex;
+
+        } else {
+
+            // Element is not found in the list
+
+            return -1;
+
+        }
 
     }
-}
+
+
+
+
+
+    }
+
